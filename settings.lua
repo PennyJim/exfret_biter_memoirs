80bf39715ca69681ddceb93be99d0a53507f9fe8
exfret <hess.kyler@gmail.com>
2024-02-11 16:24:10 -0500

    Added version 1.0.1 files
    Added everything from v1.0.1 (already released). Before this, the project was not version controlled.

---
+++ settings.lua
@@ -0,0 +1,20 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "exfret-biter-memoirs-name-chance",
+        localised_name = "Biter Name Chance",
+        minimum_value = 0,
+        maximum_value = 1,
+        setting_type = "runtime-global",
+        default_value = 0.25
+    },
+    {
+        type = "double-setting",
+        name = "exfret-biter-memoirs-message-chance",
+        localised_name = "Biter Message Chance",
+        minimum_value = 0,
+        maximum_value = 1,
+        setting_type = "runtime-global",
+        default_value = 0.03
+    }
+})
