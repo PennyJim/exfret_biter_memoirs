4e6d10effd80f8c71ba7df1ef57c2b4e02347906
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-07-12 18:32:54 -0600

    math.random(n) is inclusive

--- scripts/memoir.lua
+++ scripts/memoir.lua
@@ -11,7 +11,7 @@ function show_memoir(event)
         my_name = global.unit_info[unit_number].name
     end
     if my_name == nil then
-        my_name = global.biter_names[math.random(1, #global.biter_names)]
+        my_name = global.biter_names[math.random(#global.biter_names)]
     end
 
     ---@type string
@@ -20,14 +20,14 @@ function show_memoir(event)
     if global.biter_memoirs_special[my_name] then
         locale_key = "biter-memoirs-special."..my_name
     else
-        locale_key = "biter-memoirs."..math.random(1, global.biter_memoir_count)
+        locale_key = "biter-memoirs."..math.random(global.biter_memoir_count)
     end
 
     local pronouns = global.biter_name_pronouns[my_name] or "their"
     if pronouns == "either" then
         ---@type support_pronouns[]
         local possible_pronouns = {"male", "female", "their"}
-        pronouns = possible_pronouns[math.random(1, 3)]
+        pronouns = possible_pronouns[math.random(3)]
     end
 
     ---@type LocalisedString
