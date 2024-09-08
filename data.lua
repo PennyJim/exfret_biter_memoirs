9de03e6eb4a72321023dd794545c0526aaa36c60
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-09-08 17:56:29 -0600

    Use rendering

--- data.lua
+++ data.lua
@@ -1,14 +1,3 @@
-data.raw["flying-text"]["biter-name"] = {
-    name = "biter-name",
-    type = "flying-text",
-    flags = {
-        "not-on-map",
-        "placeable-off-grid"
-    },
-    speed = 0,
-    time_to_live = 2
-}
-
 data:extend({
     {
         type = "custom-input",
--- scripts/initialize_unit.lua
+++ scripts/initialize_unit.lua
@@ -1,17 +1,29 @@
 ---@param event {entity:LuaEntity, keep_hidden:boolean}
 function initialize_unit(event)
-    if event.entity.type == "unit" then
-        local biter_name = global.biter_names[math.random(1, #global.biter_names)]
-        local show_name = false
-        if (not event.keep_hidden) and math.random() < settings.global["exfret-biter-memoirs-name-chance"].value then
-            show_name = true
-        end
+    local entity = event.entity
+    if entity.type ~= "unit" then return end
 
-        global.unit_info[event.entity.unit_number--[[@as integer]]] = {
-            name = biter_name,
-            show_name = show_name,
-            entity = event.entity,
-            birth = game.tick
+    local biter_name = global.biter_names[math.random(1, #global.biter_names)]
+    local show_name, renderID = false, nil
+
+    if (not event.keep_hidden)
+    and math.random() < settings.global["exfret-biter-memoirs-name-chance"].value then
+        show_name = true
+        renderID = rendering.draw_text{
+            text = biter_name,
+            color = {1,1,1},
+            surface = entity.surface_index,
+            target = entity,
+            alignment = "center",
+            vertical_alignment = "bottom",
+            use_rich_text = true,
         }
     end
+
+    global.unit_info[entity.unit_number--[[@as integer]]] = {
+        name = biter_name,
+        show_name = show_name,
+        entity = event.entity,
+        birth = game.tick
+    }
 end
